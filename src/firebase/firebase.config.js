58cabd8ee31471ca7eba81b75f11b20ea188b009
Toufique18 <toufiqueeamam7@gmail.com>
2024-06-12 13:56:49 +0600

    firebase setup  and commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUn7_3NL_KkQmSwaXhQlRUH8fOE0OwfGI",
+  authDomain: "contesthub-d205f.firebaseapp.com",
+  projectId: "contesthub-d205f",
+  storageBucket: "contesthub-d205f.appspot.com",
+  messagingSenderId: "807458096911",
+  appId: "1:807458096911:web:8913ec2bc6649c58b7207a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
